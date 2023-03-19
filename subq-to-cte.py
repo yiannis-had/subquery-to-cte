e38a6c2087d73306863ed45b724a85d8ab9f085f
Yiannis Hadjicharalambous <hadjicharalambous.yiannis@gmail.com>
2023-03-19 12:13:11 +0000

    got the data right,just need to diplay the qry now

--- subq-to-cte.py
+++ subq-to-cte.py
@@ -1,29 +1,47 @@
 import sqlvalidator
-from sqlvalidator.grammar.sql import Parenthesis, SelectStatement, Table
+from sqlvalidator.grammar.sql import Parenthesis, SelectStatement, WithStatement, Table, Alias
+from collections import deque
 
 sql_query = sqlvalidator.parse("""
-select *, row_number() over(partition by a order by b desc) row_num from (select a,b,c from  (select * from table_x where family = 'Smiths') )
+select *, row_number() over(partition by a order by b desc) row_num from (select a,b,c from  (select * from table_x where family = 'Smiths';)a )b
 """)
 
-def graph_it(sql_query):
+def graph_it(sql_query: SelectStatement | WithStatement):
     nodes = {}
     edges = {}
+    edges_stack = []
     def parse_tree(sql_query, counter=0):
-        nodes[counter] = sql_query
-        if isinstance(sql_query.from_statement, Parenthesis) and isinstance(sql_query.from_statement.args[0], SelectStatement):
-            edges[counter] = counter + 1
+        print(sql_query.__repr__())
+        print('-------------------')
+        if isinstance(sql_query, Alias) and isinstance(sql_query.expression, Parenthesis):
+            alias = sql_query.alias
+            sql_query = sql_query.expression
+        else:
+            alias = None
+        if isinstance(sql_query, Parenthesis) and isinstance(sql_query.args[0], SelectStatement):
+            sql_query = sql_query.args[0]
+        key = alias if alias else counter
+        nodes[key] = sql_query
+        if edges_stack:
+            prev_key = edges_stack.pop()
+            edges[prev_key] = key
+        edges_stack.append(key)
+        if alias is None:
             counter += 1
-            return parse_tree(sql_query.from_statement.args[0], counter)
+        if hasattr(sql_query, 'from_statement') and not isinstance(sql_query.from_statement, Table):
+            return parse_tree(sql_query.from_statement, counter)
         return (nodes, edges)
     return parse_tree(sql_query)
 
 nodes, edges = graph_it(sql_query.sql_query)
+nodes_keys = list(nodes.keys())
 
-for index, node in enumerate(reversed(nodes), start=1):
+for index, node in enumerate(reversed(nodes_keys)):
+    node_index = None if len(nodes_keys) - index else None
     select_statement: SelectStatement = nodes[node]
     innermost_select = node not in edges
-    outermost_select = node == list(nodes.keys())[0]
-    before_outermost_select = node == list(nodes.keys())[1]
+    outermost_select = node == nodes_keys[0]
+    before_outermost_select = node == nodes_keys[1]
     if not innermost_select:
         select_statement.from_statement = Table(f"cte_{index-1}")
 
